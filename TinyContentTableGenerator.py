51a5711f4e9b1a0b5c178c13807521bb00c388a0
Harry <checkhokan22@gmail.com>
2021-12-07 19:54:12 +0800

    update

--- TinyContentTableGenerator.py
+++ TinyContentTableGenerator.py
@@ -5,7 +5,7 @@ import os , re
 # It is quite messy to manage content table manually , so a tiny script is written to handle the issue.
 
 dirs = [directory for directory in os.listdir(os.curdir) if isdir(os.curdir + '\\' + directory)]
-file = open('README_test.md' , 'w')
+file = open('README.md' , 'w')
 file.write('# LeetCodeRepo\nThis is my personal LeetCodeRepo.\nFeel free to look around.\n\n\n## Contents\n|  \\#   | Title  |\n|  ----  | ----  |\n')
 URL_root = 'https://github.com/CheckHarry/LeetCodeRepo/tree/main/'
 dirs.remove('.git')
