b202ee26463c253d4e09818563c4703240ce30f9
Harry <checkhokan22@gmail.com>
2021-12-23 00:40:53 +0800

    Update RandomizedCollection.cpp

--- 381. Insert Delete GetRandom O(1) - Duplicates allowed/RandomizedCollection.cpp
+++ 381. Insert Delete GetRandom O(1) - Duplicates allowed/RandomizedCollection.cpp
@@ -25,7 +25,75 @@ public:
             it--;
             int temp = *it;
         }
+        else return false;#include <unordered_map>
+#include <unordered_set>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+
+
+
+
+class RandomizedCollection {
+public:
+    RandomizedCollection() {
+
+    }
+
+    bool insert(int val) {
+        nums_vec.push_back(val);
+        if (multiset.find(val) == multiset.end()) { multiset.insert(pair<int, unordered_set<unsigned int>>(val, { (unsigned int)(nums_vec.size() - 1 )})); return true; }
+        else { (*multiset.find(val)).second.insert(nums_vec.size() - 1); return false; }
+        return true;
+    }
+
+    bool remove(int val) {
+        if (multiset.find(val) != multiset.end()) {
+            unordered_set<unsigned int>& vec = (*multiset.find(val)).second;
+            unordered_set<unsigned int>::iterator it = vec.begin();
+            int temp = *it;
+            vec.erase(it);
+            if (vec.empty()) multiset.erase(val);
+            if (temp != nums_vec.size() - 1) {
+                nums_vec[temp] = nums_vec[nums_vec.size() - 1];
+                (*multiset.find(nums_vec[nums_vec.size() - 1])).second.erase(nums_vec.size() - 1);
+                (*multiset.find(nums_vec[nums_vec.size() - 1])).second.insert(temp);
+            }
+            vector<int>::iterator it_vec = nums_vec.end();
+            it_vec--;
+            nums_vec.erase(it_vec);
+          
+            return true;
+        }
         else return false;
+    }
+
+    int getRandom() {
+        return nums_vec[rand() % nums_vec.size()];
+    }
+private:
+    unordered_map<int, unordered_set<unsigned int>> multiset;
+    vector<int> nums_vec;
+};
+
+
+
+int main() {
+    RandomizedCollection RC;
+    RC.insert(1);
+    RC.insert(1);
+    RC.insert(2);
+    RC.remove(1);
+   
+    int count1 = 0, count2 = 0;
+    for (int i = 0; i < 100000; i++) {
+        if (RC.getRandom() == 1) count1++;
+        else count2++;
+    }
+    cout << count1 << '\n';
+    cout << count2 << '\n';
+}
     }
 
     int getRandom() {
