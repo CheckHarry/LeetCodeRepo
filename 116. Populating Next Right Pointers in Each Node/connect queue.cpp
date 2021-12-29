548bbb5b149a187d93d8ec7c79d1107a84627bfb
CheckHarry <81646293+CheckHarry@users.noreply.github.com>
2021-12-29 15:10:12 +0800

    Create connect queue.cpp

---
+++ 116. Populating Next Right Pointers in Each Node/connect queue.cpp
@@ -0,0 +1,22 @@
+
+
+
+
+Node* connect(Node* root) {
+        queue<Node*> layer;
+        int nodes_num = 1;
+        layer.push(root);
+        for(;layer.front() != NULL;nodes_num *= 2){
+         for (int i = 0 ; i < nodes_num - 1; i ++){
+                Node* temp = layer.front();
+                layer.push(temp -> left);
+                layer.push(temp -> right);
+                layer.pop();
+                temp -> next = layer.front();
+            }
+            layer.push(layer.front() -> left);
+            layer.push(layer.front() -> right);
+            layer.pop();
+        }
+        return root;
+    }
