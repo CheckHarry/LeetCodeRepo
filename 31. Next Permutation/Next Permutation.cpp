60edd7dd4a9b2ac43cba648f900e93efe22fb922
Harry <checkhokan22@gmail.com>
2022-01-07 16:13:41 +0800

    update 46

--- 31. Next Permutation/Next Permutation.cpp
+++ 31. Next Permutation/Next Permutation.cpp
@@ -14,7 +14,7 @@ bool nextPermute(vector<int>& nums , int size , int len) {
 		if (is_change) return true;
 		else if (nums[size - len] < nums[size - len + 1]){
 			for (int index = size - len + 1; index < size + 1; index++) 
-				if (index == size or nums[size - len] >= nums[index]) {
+				if (index == size || nums[size - len] >= nums[index]) {
 					int temp = nums[index - 1];
 					nums[index - 1]  = nums[size - len];
 					nums[size - len] = temp;
---
+++ 46. Permutations/permute.cpp
@@ -0,0 +1,51 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+
+
+
+void dfs(vector<int> &nums ,vector<bool> &mask , vector<vector<int>>& to_return ,  vector<int>& vec ,int pos){
+    if (pos == nums.size() - 1){
+        for (int i = 0 ; i < nums.size() ; i ++) {
+            if (mask[i] == false) {
+                vec.push_back(nums[i]);
+                to_return.push_back(vec);
+                vec.pop_back();
+                break;
+            }
+        }
+    }
+    else{
+        for (int i = 0 ; i < nums.size() ; i ++){
+            if (mask[i] == false){
+                mask[i] = true;
+                vec.push_back(nums[i]);
+                if (pos == nums.size() - 1) to_return.push_back(vec);
+                else dfs(nums , mask , to_return , vec , pos + 1);
+                mask[i] = false;
+                vec.pop_back();
+                if (pos == nums.size() - 1) break;
+            }
+        }
+    }
+}
+
+vector<vector<int>> permute(vector<int> &nums){
+    vector<bool> mask(21 , false);
+    vector<vector<int>> to_return;
+    vector<int> vec;
+    int pos = 0;
+    dfs(nums , mask , to_return , vec , pos);
+    return to_return;
+}
+
+int main(){
+    vector<int> x{1,2,3,4};
+    vector<vector<int>> t = permute(x);
+    for (vector<int> vec : t){
+        for (int i : vec) cout << i << ',';
+        cout << '\n';
+    }
+}
