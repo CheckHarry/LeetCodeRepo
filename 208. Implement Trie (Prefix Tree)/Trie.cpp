8a9a0f7b9e53d22c1facc249b310b400696ae097
Harry <checkhokan22@gmail.com>
2022-01-28 21:25:38 +0800

    update 208 211

---
+++ 208. Implement Trie (Prefix Tree)/Trie.cpp
@@ -0,0 +1,68 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+
+
+struct TrieNode{
+    char value;
+    bool is_end = false;
+    vector<TrieNode*> vec{26 , nullptr};
+};
+
+
+class Trie {
+private:
+    TrieNode* root;
+public:
+    Trie() {
+        root = new TrieNode;
+    }
+    
+    void insert(string word) {
+        TrieNode* cur = root;
+        int count = 0;
+        for (char x : word){
+            count ++;
+            if (cur -> vec[ x - 'a'] != nullptr) cur = cur -> vec[ x - 'a'];
+            else{
+                TrieNode* newptr = new TrieNode;
+                newptr -> value = x;
+                cur -> vec[x - 'a'] = newptr;
+                cur = newptr;
+            }
+            if (count == word.size()) cur -> is_end = true;
+        }
+    }
+    
+    bool search(string word) {
+        TrieNode* cur = root;
+        for (char x : word){
+            if (cur -> vec[ x - 'a'] != nullptr) cur = cur -> vec[ x - 'a'];
+            else return false;
+        }
+        return cur -> is_end;
+    }
+    
+    bool startsWith(string prefix) {
+        TrieNode* cur = root;
+        for (char x : prefix){
+            if (cur -> vec[ x - 'a'] != nullptr) cur = cur -> vec[ x - 'a'];
+            else return false;
+        }
+        return true;
+    }
+};
+
+
+int main(){
+    Trie x = Trie();
+    x.insert("abc");
+    cout << x.startsWith("abc") << '\n';
+    cout << x.search("abc") << '\n';
+    cout << x.startsWith("ab") << '\n';
+    cout << x.search("ab") << '\n';
+    x.insert("ab");
+    cout << x.search("ab") << '\n';
+}
---
+++ 211. Design Add and Search Words Data Structure/WordDictionary.cpp
@@ -0,0 +1,69 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+
+
+struct TrieNode{
+    char value;
+    bool is_end = false;
+    vector<TrieNode*> vec{26 , nullptr};
+};
+
+
+class WordDictionary {
+private:
+    TrieNode* root;
+public:
+    WordDictionary() {
+        root = new TrieNode;
+    }
+    
+    void insert(string word) {
+        TrieNode* cur = root;
+        int count = 0;
+        for (char x : word){
+            count ++;
+            if (cur -> vec[ x - 'a'] != nullptr) cur = cur -> vec[ x - 'a'];
+            else{
+                TrieNode* newptr = new TrieNode;
+                newptr -> value = x;
+                cur -> vec[x - 'a'] = newptr;
+                cur = newptr;
+            }
+            if (count == word.size()) cur -> is_end = true;
+        }
+    }
+    
+    bool search(string word) {
+        vector<TrieNode*> cur_nodes;
+        cur_nodes.push_back(root);
+        for (char x : word){
+            vector<TrieNode*> next_nodes;
+            for (TrieNode* node : cur_nodes){
+                if (x == '.'){
+                    for (TrieNode* child : node -> vec) if (child != nullptr) next_nodes.push_back(child);
+                }
+                else{
+                    if (node -> vec[x - 'a'] != nullptr) next_nodes.push_back(node -> vec[x - 'a']);
+                }
+            }
+            cur_nodes = next_nodes;
+        }
+        
+        for (TrieNode* node : cur_nodes){
+            if (node -> is_end) return true;
+        }
+        return false;
+    }
+};
+
+
+int main(){
+    WordDictionary x;
+    x.insert("abcd");
+    cout << x.search("a.c.") << '\n';
+    cout << x.search("ab..") << '\n';
+    cout << x.search("ab.") << '\n';
+}
