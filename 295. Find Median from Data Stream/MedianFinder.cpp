6c00d9855a6193c6ebcecdb7a125f7ad422d847e
harrycheck <checkhokan22@gmail.com>
2022-02-14 21:51:43 +0800

    update 295

--- 295. Find Median from Data Stream/MedianFinder.cpp
+++ 295. Find Median from Data Stream/MedianFinder.cpp
@@ -1,23 +1,40 @@
 #include <queue>
 #include <vector>
+#include <iostream>
 using namespace std;
 
 
 
-
+auto cmp = [](int x , int y){return x > y;};
 class MedianFinder {
 private:
     int size;
-    auto cmp = [](int x , int y){return x > y;};
-    priority_queue<int> left();
-    priority_queue<int , vector<int> , decltype(cmp)> right(cmp);
+    priority_queue<int> left;
+    priority_queue<int , vector<int> , decltype(cmp)> right{cmp};
 public:
     MedianFinder() : size(0){
-        
+        left.push(-100001);
+        right.push(100001);
     }
     
     void addNum(int num) {
-        
+       if (size % 2 == 0) {
+           if (num <= left.top()){
+               right.push(left.top());
+               left.pop();
+               left.push(num);
+           }
+           else right.push(num);
+       }
+       else{
+           if (num >= right.top()){
+                left.push(right.top());
+                right.pop();
+                right.push(num);
+           }
+           else left.push(num);
+       }
+       size ++;
     }
     
     double findMedian() {
@@ -27,3 +44,10 @@ public:
         }
     }
 };
+
+int main(){
+    MedianFinder MF;
+    MF.addNum(1);
+
+    cout << MF.findMedian();
+}
